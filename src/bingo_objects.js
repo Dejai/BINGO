36dc17d755b470b87706f1349948b6165e3fc840
Derrick Fyfield <dfyfield3@gatech.edu>
2022-02-12 15:34:18 -0400

    Adjusting BINGO card;

--- src/bingo_objects.js
+++ src/bingo_objects.js
@@ -10,6 +10,7 @@ const bingo_letters =
 const games_object =
 	{
 		"Outer Box":{
+			"group": "Classic Games",
 			"desc": "The game is Outer Box!",
 			"example": 	[
 							[1,1,1,1,1],
@@ -20,6 +21,7 @@ const games_object =
 						]
 		},
 		"Inner Box": {
+			"group": "Classic Games",
 			"desc": "The game is Inner Box!",
 			"ignore": ["B", "O"],
 			"example": 	[
@@ -30,7 +32,119 @@ const games_object =
 							[0,0,0,0,0]
 						]
 		},
+		"Straight Line": {
+			"group": "Classic Games",
+			"desc": "The game is any Straight Line! Any vertical, horizontal, or diagonal line of 5!",
+			"example": 	[]
+		},
+		"Full House": {
+			"group": "Classic Games",
+			"desc": "The game is Full House! All the spaces on your card.",
+			"example": 	[
+							[1,1,1,1,1],
+							[1,1,1,1,1],
+							[1,1,8,1,1],
+							[1,1,1,1,1],
+							[1,1,1,1,1]
+						]
+		},
+		"Popsicle": {
+			"group": "Random",
+			"desc": "The game is Popsicle! The shape looks like a popsicle.",
+			"ignore": ["B", "O"],
+			"example": 	[
+							[0,1,1,1,0],
+							[0,1,1,1,0],
+							[0,1,8,1,0],
+							[0,0,1,0,0],
+							[0,0,1,0,0]
+						]
+		},
+		"The Cross": {
+			"group": "Random",
+			"desc": "The game is The Cross! Down the N, and across the middle row.",
+			"example": 	[
+							[0,0,1,0,0],
+							[0,0,1,0,0],
+							[1,1,8,1,1],
+							[0,0,1,0,0],
+							[0,0,1,0,0]
+						]
+		},
+		"Greater Than": {
+			"group": "Random",
+			"desc": "The game is Greater Than! The greater than symbol.",
+			"ignore": ["B", "I"],
+			"example": 	[
+							[1,0,0,0,0],
+							[0,1,0,0,0],
+							[0,0,8,0,0],
+							[0,1,0,0,0],
+							[1,0,0,0,0]
+						]
+		},
+		"Less Than": {
+			"group": "Random",
+			"desc": "The game is Less Than! The less than symbol.",
+			"ignore": ["B", "I"],
+			"example": 	[
+							[0,0,0,0,1],
+							[0,0,0,1,0],
+							[0,0,8,0,0],
+							[0,0,0,1,0],
+							[0,0,0,0,1]
+						]
+		},
+		
+		"The Ladder": {
+			"group": "Random",
+			"desc": "The game is The Ladder! Across the top row, middle row, and bottom row",
+			"example": 	[
+							[1,1,1,1,1],
+							[0,0,0,0,0],
+							[1,1,8,1,1],
+							[0,0,0,0,0],
+							[1,1,1,1,1]
+						]
+		},
+		"Stripes": {
+			"group": "Random",
+			"desc": "The game is Vertical Stripes! Down the B, N, and O",
+			"ignore": ["I", "G"],
+			"example": 	[
+							[1,0,1,0,1],
+							[1,0,1,0,1],
+							[1,0,8,0,1],
+							[1,0,1,0,1],
+							[1,0,1,0,1]
+						]
+		},
+		"The Grid!": {
+			"group": "Random",
+			"desc": "The game is The Grid!! The 1st, 3rd, and last colmns as well as rows.",
+			"ignore": ["I", "G"],
+			"example": 	[
+							[1,1,1,1,1],
+							[1,0,1,0,1],
+							[1,1,8,1,1],
+							[1,0,1,0,1],
+							[1,1,1,1,1]
+						]
+		},
+		"Train Tracks": {
+			"group": "Random",
+			"desc": "The game is Train Tracks! Down the I and G",
+			"ignore": ["B", "N", "O"],
+			"example": 	[
+							[0,1,0,1,0],
+							[0,1,0,1,0],
+							[0,1,3,1,0],
+							[0,1,0,1,0],
+							[0,1,0,1,0]
+						]
+		},
 		"Letter: A":{
+			"group": "Letters",
 			"desc": "The game is the Letter A!",
 			"example": 	[
 							[1,1,1,1,1],
@@ -41,6 +155,7 @@ const games_object =
 						]
 		},
 		"Letter: B":{
+			"group": "Letters",
 			"desc": "The game is the Letter B!",
 			"example": 	[
 							[1,1,1,1,1],
@@ -51,6 +166,7 @@ const games_object =
 						]
 		},
 		"Letter: C":{
+			"group": "Letters",
 			"desc": "The game is the Letter C!",
 			"example": 	[
 							[1,1,1,1,1],
@@ -61,6 +177,7 @@ const games_object =
 						]
 		},
 		"Letter: E": {
+			"group": "Letters",
 			"desc": "The game is the Letter E!",
 			"example": 	[
 							[1,1,1,1,1],
@@ -71,6 +188,7 @@ const games_object =
 						]
 		},
 		"Letter: F":{
+			"group": "Letters",
 			"desc": "The game is the Letter F! F is for Fy-field!",
 			"example": 	[
 							[1,1,1,1,1],
@@ -82,6 +200,7 @@ const games_object =
 
 		},
 		"Letter: H": {
+			"group": "Letters",
 			"desc": "The game is the Letter H!",
 			"ignore": ["N"],
 			"example": 	[
@@ -93,6 +212,7 @@ const games_object =
 						]
 		},
 		"Letter: I": {
+			"group": "Letters",
 			"desc": "The game is the Letter I!",
 			"example": 	[
 							[1,1,1,1,1],
@@ -103,6 +223,7 @@ const games_object =
 						]
 		},
 		"Letter: L": {
+			"group": "Letters",
 			"desc": "The game is the Letter L!",
 			"example": 	[
 							[1,0,0,0,0],
@@ -113,6 +234,7 @@ const games_object =
 						]
 		},
 		"Letter: M": {
+			"group": "Letters",
 			"desc": "The game is the Letter M!",
 			"ignore": ["N"],
 			"example": 	[
@@ -124,6 +246,7 @@ const games_object =
 						]
 		},
 		"Letter: N": {
+			"group": "Letters",
 			"desc": "The game is the Letter N!",
 			"ignore": ["N"],
 			"example": 	[
@@ -135,6 +258,7 @@ const games_object =
 						]
 		},
 		"Letter: P": {
+			"group": "Letters",
 			"desc": "The game is the Letter P!",
 			"example": 	[
 							[1,1,1,1,1],
@@ -145,6 +269,7 @@ const games_object =
 						]
 		},
 		"Letter: S": {
+			"group": "Letters",
 			"desc": "The game is the Letter S!",
 			"example": 	[
 							[1,1,1,1,1],
@@ -155,6 +280,7 @@ const games_object =
 						]
 		},
 		"Letter: T": {
+			"group": "Letters",
 			"desc": "The game is the Letter T!",
 			"example": 	[
 							[1,1,1,1,1],
@@ -165,6 +291,7 @@ const games_object =
 						]
 		},
 		"Letter: U": {
+			"group": "Letters",
 			"desc": "The game is the Letter U!",
 			"example": 	[
 							[1,0,0,0,1],
@@ -175,6 +302,7 @@ const games_object =
 						]
 		},
 		"Letter: W": {
+			"group": "Letters",
 			"desc": "The game is the Letter W!",
 			"ignore": ["N"],
 			"example": 	[
@@ -186,6 +314,7 @@ const games_object =
 						]
 		},
 		"Letter: X": {
+			"group": "Letters",
 			"desc": "The game is the Letter X!",
 			"ignore": ["N"],
 			"example": 	[
@@ -197,6 +326,7 @@ const games_object =
 						]
 		},
 		"Letter: Y": {
+			"group": "Letters",
 			"desc": "The game is the Letter Y!",
 			"example": 	[
 							[1,0,0,0,1],
@@ -207,26 +337,13 @@ const games_object =
 						]
 		},
 		"Letter: Z": {
+			"group": "Letters",
 			"desc": "The game is the Letter Z!",
 			"example": 	[
 							[1,1,1,1,1],
 							[0,0,0,1,0],
 							[0,0,8,0,0],
 							[0,1,0,0,0],
-							[1,1,1,1,1]
-						]
-		},
-		"Straight Line": {
-			"desc": "The game is any Straight Line! Any vertical, horizontal, or diagonal line of 5!",
-			"example": 	[]
-		},
-		"Full House": {
-			"desc": "The game is Full House! All the spaces on your card.",
-			"example": 	[
-							[1,1,1,1,1],
-							[1,1,1,1,1],
-							[1,1,8,1,1],
-							[1,1,1,1,1],
 							[1,1,1,1,1]
 						]
 		}
